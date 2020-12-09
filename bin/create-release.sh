dfc70f88a4ea52539e273fde28b05eaf508399ee
Hiroshi Urabe <urabe@ham.works>
2020-12-10 00:36:49 +0900

    add release-it config

---
+++ bin/create-release.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ $# -lt 2 ]; then
+	echo "usage: $0 <plugin-name> <version>"
+	exit 1
+fi
+
+pluginname=$1
+version=$2
+
+if [ ! `echo $version | grep -e 'alpha' -e 'beta' -e 'RC' -e 'rc'` ] ; then
+	sed -i.bak -e "s/^Stable tag: .*/Stable tag:        ${version}/g" README.md;
+	rm README.md.bak
+fi
+
+sed -i.bak -e "s/^ \* Version: .*/ * Version: ${version}/g" ${pluginname}.php;
+sed -i.bak -e "s/^ \* @version .*/ * @version ${version}/g" ${pluginname}.php;
+rm ${pluginname}.php.bak
+
+SCRIPT_DIR=$(dirname $0);
+
+. ${SCRIPT_DIR}/build.sh
+
+rsync -a --exclude-from=.distignore ./ ./distribution/
