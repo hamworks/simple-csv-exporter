9007036e3c20b31708a4d787fe528d2f41b47bc0
Hiroshi Urabe <urabe@ham.works>
2020-12-11 10:24:07 +0900

    Release 0.0.5

--- simple-csv-exporter.php
+++ simple-csv-exporter.php
@@ -9,7 +9,7 @@
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:     wp-csv-exporter
  * Domain Path:     /languages
- * Version: 1.0.0-beta.2
+ * Version: 0.0.5
  */
 
 use HAMWORKS\WP\Simple_CSV_Exporter\Simple_CSV_Exporter;
