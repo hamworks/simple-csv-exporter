065888cbcab0ed03b3178fbbb37feb82f43b4cd4
Hiroshi Urabe <urabe@ham.works>
2020-12-27 01:38:55 +0900

    update DI config.

--- src/Simple_CSV_Exporter.php
+++ src/Simple_CSV_Exporter.php
@@ -3,8 +3,8 @@
 namespace HAMWORKS\WP\Simple_CSV_Exporter;
 
 use DI\ContainerBuilder;
-use Psr\Container\ContainerInterface;
 use function DI\autowire;
+use function DI\create;
 use function DI\get;
 
 /**
@@ -19,17 +19,18 @@ class Simple_CSV_Exporter {
 		$builder = new ContainerBuilder();
 		$builder->addDefinitions(
 			array(
-				'var.name'      => 'post_type_to_export',
-				'slug'          => 'simple_csv_exporter',
-				Nonce::class    => function ( ContainerInterface $c ) {
-					return new Nonce( $c->get( 'slug' ) );
-				},
-				Admin_UI::class => autowire()->constructor( get( 'slug' ), get( 'var.name' ) ),
-				Exporter::class => autowire()->constructor( get( 'slug' ), get( 'var.name' ) ),
+				'var.name'          => 'post_type_to_export',
+				'slug'              => 'simple_csv_exporter',
+				Nonce::class        => create()->constructor( get( 'slug' ) ),
+				Data_Builder::class => create( Data_Builder_For_WP_Posts::class )
+					->constructor( filter_input( INPUT_POST, get( 'var.name' ), FILTER_SANITIZE_STRING ) ),
+				Admin_UI::class     => autowire()->constructor( get( 'slug' ), get( 'var.name' ) ),
+				Exporter::class     => autowire()->constructor( get( 'slug' ), get( 'var.name' ) ),
 			)
 		);
 
 		$container = $builder->build();
+
 		$container->get( Admin_UI::class );
 		$container->get( Exporter::class );
 	}
