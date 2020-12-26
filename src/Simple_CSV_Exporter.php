370d8bc6ab292baa369c52ccc61ac5b6b925fa1b
Hiroshi Urabe <urabe@ham.works>
2020-12-27 00:44:47 +0900

    use PHP-DI

--- src/Simple_CSV_Exporter.php
+++ src/Simple_CSV_Exporter.php
@@ -2,36 +2,38 @@
 
 namespace HAMWORKS\WP\Simple_CSV_Exporter;
 
+use DI\ContainerBuilder;
+use Psr\Container\ContainerInterface;
+
 /**
  * Class Init
  */
 class Simple_CSV_Exporter {
 
-	const POST_TYPE_TO_EXPORT = 'post_type_to_export';
-	const SLUG                = 'simple_csv_exporter';
-
-	/**
-	 * @var Nonce
-	 */
-	private $nonce;
-
-	/**
-	 * @var Admin_UI
-	 */
-	private $admin_ui;
-
-	/**
-	 * @var Exporter
-	 */
-	private $exporter;
-
 	/**
 	 * Admin constructor.
 	 */
 	public function __construct() {
-		$this->nonce    = new Nonce( self::SLUG );
-		$this->admin_ui = new Admin_UI( self::SLUG, self::POST_TYPE_TO_EXPORT, $this->nonce );
-		$this->exporter = new Exporter( self::SLUG, self::POST_TYPE_TO_EXPORT, $this->nonce );
+		$builder = new ContainerBuilder();
+		$builder->addDefinitions(
+			array(
+				'var.name'      => 'post_type_to_export',
+				'slug'          => 'simple_csv_exporter',
+				Nonce::class    => function ( ContainerInterface $c ) {
+					return new Nonce( $c->get( 'slug' ) );
+				},
+				Admin_UI::class => function ( ContainerInterface $c ) {
+					return new Admin_UI( $c->get( 'slug' ), $c->get( 'var.name' ), $c->get( Nonce::class ) );
+				},
+				Exporter::class => function ( ContainerInterface $c ) {
+					return new Exporter( $c->get( 'slug' ), $c->get( 'var.name' ), $c->get( Nonce::class ) );
+				},
+			)
+		);
+
+		$container = $builder->build();
+		$container->get( Admin_UI::class );
+		$container->get( Exporter::class );
 	}
 
 
