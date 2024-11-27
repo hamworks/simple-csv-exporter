fffdd02e7cbd52367b039c2a7e863221c007272e
Hiroshi Urabe <mail@torounit.com>
2024-11-27 18:46:03 +0900

    set require php version

--- simple-csv-exporter.php
+++ simple-csv-exporter.php
@@ -9,7 +9,7 @@
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:     wp-csv-exporter
  * Domain Path:     /languages
- * Requires PHP:    7.4
+ * Requires PHP:    8.0
  * Version: 2.1.8
  */
 
