1647e41685a83a08ee27c14f5f80059bf1ceb88d
Hiroshi Urabe <mail@torounit.com>
2024-11-29 15:43:23 +0900

    fix encoding

--- src/Encodings.php
+++ src/Encodings.php
@@ -7,15 +7,19 @@ namespace HAMWORKS\WP\Simple_CSV_Exporter;
  */
 class Encodings {
 
+	public const UTF8 = array(
+		'name'  => 'UTF-8',
+		'value' => 'UTF-8',
+	);
+
+	public const UTF8_WITH_BOM = array(
+		'name'  => 'UTF8_WITH_BOM',
+		'value' => 'UTF-8 with BOM',
+	);
+
 	const ITEMS = array(
-		array(
-			'name'  => 'UTF-8',
-			'value' => 'UTF-8',
-		),
-		array(
-			'name'  => 'UTF8_WITH_BOM',
-			'value' => 'UTF-8 with BOM',
-		),
+		self::UTF8,
+		self::UTF8_WITH_BOM,
 	);
 
 	public static function get_items(): array {
