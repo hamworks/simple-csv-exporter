a7cc4828d8621c35a004ee89e103d4f109f2a21b
Hiroshi Urabe <mail@torounit.com>
2024-11-29 16:09:01 +0900

    fix test

--- tests/CSV_Writer_Test.php
+++ tests/CSV_Writer_Test.php
@@ -38,7 +38,9 @@ A,B
 CSV;
 
 		$this->expectOutputString( $expect );
-		$csv = new CSV_Writer( $data, 'php://output' );
+		$csv = new CSV_Writer();
+		$csv->set_file_name( 'php://output' );
+		$csv->set_rows( $data );
 		$csv->write( $data );
 	}
 
@@ -69,8 +71,9 @@ A,B
 
 CSV;
 
-		$csv = new CSV_Writer( $data, '/tmp/test.csv' );
-		$csv->write( $data );
+		$csv = new CSV_Writer();
+		$csv->set_file_name( 'php://output' );
+		$csv->set_rows( $data );
 		$this->assertFileExists( '/tmp/test.csv' );
 		$this->assertIsReadable( '/tmp/test.csv' );
 		$this->assertStringEqualsFile( '/tmp/test.csv', $expect );
