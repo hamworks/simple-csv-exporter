88de4ece8ebcf28d3a21e010279bea06f1334424
Hiroshi Urabe <mail@torounit.com>
2024-11-29 18:17:12 +0900

    Release 3.0.1

--- simple-csv-exporter.php
+++ simple-csv-exporter.php
@@ -10,7 +10,7 @@
  * Text Domain:     wp-csv-exporter
  * Domain Path:     /languages
  * Requires PHP:    8.0
- * Version: 3.0.0
+ * Version: 3.0.1
  */
 
 use HAMWORKS\WP\Simple_CSV_Exporter\Simple_CSV_Exporter;
